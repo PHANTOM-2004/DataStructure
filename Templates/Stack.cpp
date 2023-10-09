63eee6806db3e83d0ef42dd2dc76661465f2cc74
Chen YT <phantom190514@gmail.com>
2023-10-09 09:43:08 +0800

    add template Stack

--- Templates/Stack.cpp
+++ Templates/Stack.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 
-template <typename T>
-class Stack
-{
-    T *_base;
-    T *_top;
-    int _size;
-    inline int size()
-    {
-        return size;
-    }
-    inline void push(const T &elem)
-    {
-    }
+const int STACK_SIZE = 100;
+template <typename T> class Stack {
+  T *_base;
+  T *_top;
+  int _size;
+
+public:
+  Stack(const int &size = STACK_SIZE);
+  inline int size() { return _top - _base; }
+  inline bool empty() { return _top == _base; }
+  inline void push(const T &elem);
+  inline T pop() { return *--_top; }
 };
+
+template <typename T> Stack<T>::Stack(const int &size) {
+  _base = new T[size];
+  _top = _base;
+  _size = size;
+}
+
+template <typename T> void Stack<T>::push(const T &elem) {
+  if (_top - _base == _size) {
+    T *newmem = new T[_size << 1];
+    memcpy(newmem, _base, _size * sizeof(T));
+    delete[] _base;
+    _base = newmem;
+    _top = _base + _size;
+    _size *= 2;
+  }
+  *_top++ = elem;
+}
+
+int main() {
+  Stack<int> S(4);
+  for (int i = 0; i < 10; i++)
+    S.push(i);
+  for (int i = 0; !S.empty(); i++) {
+    std::cout << S.pop() << " ";
+  }
+  return 0;
+}
